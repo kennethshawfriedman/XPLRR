39d4897e7476416813329099cfc78712b12f43dd
Kenny Friedman <ksf@mit.edu>
2014-07-02 18:27:15 -0400

    Xcode project added

---
+++ XPLRRTests/XPLRRTests.m
@@ -0,0 +1,34 @@
+//
+//  XPLRRTests.m
+//  XPLRRTests
+//
+//  Created by Kenny Friedman on 7/2/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XPLRRTests : XCTestCase
+
+@end
+
+@implementation XPLRRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
