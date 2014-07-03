c544988665646d266f595da4e70f07bdf453ed21
Kenny Friedman <ksf@mit.edu>
2014-07-02 23:12:45 -0400

    MainVC and InfiniteView classes added

---
+++ XPLRR/InfiniteView.h
@@ -0,0 +1,13 @@
+//
+//  InfiniteView.h
+//  XPLRR
+//
+//  Created by Kenny Friedman on 7/2/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfiniteView : UIView
+
+@end
---
+++ XPLRR/InfiniteView.m
@@ -0,0 +1,31 @@
+//
+//  InfiniteView.m
+//  XPLRR
+//
+//  Created by Kenny Friedman on 7/2/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import "InfiniteView.h"
+
+@implementation InfiniteView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
