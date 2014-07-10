5bf35ad05385013c093f2500cceca7e016f6265f
Kenneth Friedman <kennethshawfriedman@gmail.com>
2014-07-09 23:31:24 -0400

    InfiniteView init method

--- XPLRR/InfiniteView.m
+++ XPLRR/InfiniteView.m
@@ -14,7 +14,21 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor blueColor];
+        
+		self.contentSize = CGSizeMake(5000, self.frame.size.height);
+		
+		_visibleButtons = [[NSMutableArray alloc] init];
+		_containerView = [[UIView alloc] init];
+		[_containerView setFrame:CGRectMake(0, 0, self.contentSize.width, self.contentSize.height/2)];
+		[self addSubview: _containerView];
+		
+		//Uncomment these two lines when not in testing mode
+		/*
+		[self setShowsHorizontalScrollIndicator:NO];
+		[self setShowsVerticalScrollIndicator:NO];
+		 */
+		
+		self.backgroundColor = [UIColor blueColor];
 		[self setContentSize: CGSizeMake(2048, 1536)];
 		
     }
