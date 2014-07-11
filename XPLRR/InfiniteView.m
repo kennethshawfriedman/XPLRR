6230e1de2e0340bb16d04e44f8797255a8881145
Kenneth Friedman <kennethshawfriedman@gmail.com>
2014-07-10 23:22:42 -0400

    InfiniteView cView update
    containerView created in init

--- XPLRR/InfiniteView.m
+++ XPLRR/InfiniteView.m
@@ -16,11 +16,12 @@
 		
 		[self setContentSize: CGSizeMake(self.frame.size.width, 5000)];
 		
-		
-		_visibleButtons = [[NSMutableArray alloc] init];
 		_containerView = [[UIView alloc] init];
-		[_containerView setFrame:CGRectMake(0, 0, self.contentSize.width, self.contentSize.height/2)];
+		CGRect cFrame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+		[_containerView setFrame: cFrame];
 		[self addSubview: _containerView];
+		
+		
 		//Uncomment these two lines when not in testing mode
 		/*
 		[self setShowsHorizontalScrollIndicator:NO];
