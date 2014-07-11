d929b0f3441884670c9964f16b4e53fe30cedf38
Kenneth Friedman <kennethshawfriedman@gmail.com>
2014-07-11 15:40:28 -0400

    Added XPElements
    Easily create buttons, labels, and other UI Elements in the XPLRR style.

---
+++ XPLRR/XP UI Elements/XPElements.h
@@ -0,0 +1,13 @@
+//
+//  XPElements.h
+//  XPLRR
+//
+//  Created by Kenny Friedman on 7/11/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XPElements : NSObject
+
+@end
---
+++ XPLRR/XP UI Elements/XPElements.m
@@ -0,0 +1,13 @@
+//
+//  XPElements.m
+//  XPLRR
+//
+//  Created by Kenny Friedman on 7/11/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+#import "XPElements.h"
+
+@implementation XPElements
+
+@end
